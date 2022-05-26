521e8f9a97d8a6b0ece1edea695056148305085c
Luan <lujetix@gmail.com>
2022-05-26 10:14:51 -0300

    added modules js to import all js in head

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
+import Elements from './Helpers/Elements';
+
 //GETs in URL
 var $_GET=[];
 window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(a,name,value){$_GET[name]=value;});
 
+//https://runrun.it/pt-BR/tasks
+
 //App class
 const App = () =>{    
     alert("hello i'm working");
-    //test
+    
 };
--- script.user.js
+++ script.user.js
@@ -6,7 +6,7 @@
 // @author       You
 // @match        https://runrun.it/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=runrun.it
-// @require      https://raw.githubusercontent.com/luan-grabher/RunrunItPlus/main/App.js
+// @require      https://raw.githubusercontent.com/luan-grabher/RunrunItPlus/main/modules.js
 // @grant        none
 // ==/UserScript==
 
